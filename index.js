f08497e7a0c6f9c29db546ceef1823475cea655f
Robert Bittick <robertbittick@gmail.com>
2022-09-28 17:14:23 -0700

    commit tree 6e695a9b3ee5547afef0aeb963944d75bda74d9e

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+    const cards = [
+     "Thank you, Guadalupe, for the wonderful surprise gift!",
+     "Thank you, Ollie, for the wonderful surprise gift!",
+     "Thank you, Aki, for the wonderful surprise gift!",
+ ]; 
+function writeCards() {
+   for (let i = 0; i < 3; i++) {
+     console.log(cards[i]);
+   }
+ 
+   return cards;
+ }
+
+ function countDown() {
+    let count = [1,2,3,4,5,6,7,8,9,10,11];
+    for (let i = count.length - 1; i >= 0; i--) {
+        console.log(--count[i]);
+    }
+}
